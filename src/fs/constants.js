f8c902243387601533c6d07f417d79343f1338e5
guchigow <lanov64@yandex.kz>
2024-10-05 02:22:29 +0500

    feat: create.js done

---
+++ src/fs/constants.js
@@ -0,0 +1,8 @@
+const CONSTANTS = {
+  FILES_DIRNAME: 'files',
+  NEW_FILE_NAME: 'fresh.txt',
+  FILE_CONTENT: 'I am fresh and young',
+  ERROR_MESSAGE: 'FS operation failed',
+};
+
+export default CONSTANTS;
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,24 @@
-const create = async () => {};
+import { open } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import CONSTANTS from './constants.js';
+
+const { FILES_DIRNAME, NEW_FILE_NAME, FILE_CONTENT, ERROR_MESSAGE } = CONSTANTS;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const create = async () => {
+  try {
+    const filePath = path.resolve(__dirname, FILES_DIRNAME, NEW_FILE_NAME);
+    const file = await open(filePath, 'wx');
+    await file.writeFile(FILE_CONTENT);
+  } catch (error) {
+    if (error.code === 'EEXIST') {
+      throw new Error(ERROR_MESSAGE);
+    }
+    throw error;
+  }
+};
 
 await create();
