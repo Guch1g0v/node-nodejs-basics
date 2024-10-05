d9816af4b6cb4a6b1a7c0ab9dee8d2017b8d9b08
guchigow <lanov64@yandex.kz>
2024-10-05 12:04:42 +0500

    feat: delete.js done

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import CONSTANTS from './constants.js';
+import { checkFileExists, getFilePath, throwError, getDirname } from './utils.js';
+import { rm } from 'fs/promises';
+
+const __dirname = getDirname(import.meta.url);
+
 const remove = async () => {
-    // Write your code here 
+  const fileToRemove = getFilePath(__dirname, CONSTANTS.FILES_DIRNAME, 'fileToRemove.txt');
+  const isFileExist = await checkFileExists(fileToRemove);
+  if (!isFileExist) {
+    throwError();
+  }
+  try {
+    rm(fileToRemove);
+  } catch (error) {
+    throw error;
+  }
 };
 
 await remove();
