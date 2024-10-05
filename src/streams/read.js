1b307b95f9b0e29cbf3334211ee5110622c887f9
guchigow <lanov64@yandex.kz>
2024-10-05 16:55:34 +0500

    feat: read.js done

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import { ReadStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { EOL } from 'os';
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const file = path.resolve(__dirname, 'files', 'fileToRead.txt');
+    const fileStream = ReadStream(file);
+    fileStream.on('data', (data) => {
+      process.stdout.write(`${data}${EOL}`, 'utf8');
+    });
+  } catch (error) {
+    throw error;
+  }
 };
 
 await read();
