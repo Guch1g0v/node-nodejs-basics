f548d2c87f4c962a9d398b2edb7fe0aea7f0403e
guchigow <lanov64@yandex.kz>
2024-10-07 11:52:40 +0500

    feat: wt done

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,18 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { parentPort } from 'worker_threads';
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+const nthFibonacci = (n) => (n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2));
+
+const sendResult = (n) => {
+  return nthFibonacci(n);
 };
 
-sendResult();
+parentPort.on('message', (n) => {
+  try {
+    const data = sendResult(n);
+    parentPort.postMessage({ status: 'resolved', data });
+    process.exit(0);
+  } catch (error) {
+    parentPort.postMessage({ status: 'error', data: null });
+    process.exit(1);
+  }
+});
