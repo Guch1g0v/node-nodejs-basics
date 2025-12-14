4cc3d97d0614369f24139d626cae980047b33812
guch1g0v <lanov64@gmail.com>
2025-12-14 14:12:46 +0500

    feat: add create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,26 @@
+import path from 'node:path';
+import fs from 'node:fs/promises';
+
+const FILE_NAME = 'fresh.txt';
+const FILE_CONTENT = 'I am fresh and young';
+const ERROR_MESSAGE = 'FS operation failed';
+
+const dirname = import.meta.dirname;
+const pathToFile = path.join(dirname, 'files', FILE_NAME);
+
 const create = async () => {
-  // Write your code here
+  try {
+    const isFileExists = await fs
+      .access(pathToFile, fs.constants.F_OK)
+      .then(() => true)
+      .catch(() => false);
+    if (isFileExists) {
+      throw new Error(ERROR_MESSAGE);
+    }
+    await fs.writeFile(pathToFile, FILE_CONTENT);
+  } catch (e) {
+    throw e;
+  }
 };
 
 await create();
