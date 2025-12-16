a6e15db85b15676ffce6031385f782c33a0a3db0
guch1g0v <lanov64@gmail.com>
2025-12-17 00:56:34 +0500

    refactor: list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -6,9 +6,10 @@ import { CONSTANTS, ERRORS, isFileExists } from './helpers.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const monthFormatter = new Intl.DateTimeFormat('en', { month: 'short' });
-const dayFormatter = new Intl.DateTimeFormat('en', { day: '2-digit' });
-const timeFormatter = new Intl.DateTimeFormat('en', {
+const dateFormatter = new Intl.DateTimeFormat('en', {
+  year: 'numeric',
+  month: 'short',
+  day: '2-digit',
   hour: '2-digit',
   minute: '2-digit',
   hour12: false,
@@ -23,24 +24,20 @@ const list = async () => {
   const results = await Promise.all(
     dirents.map(async (dirent) => {
       const fullPath = path.join(targetDir, dirent.name);
-      const stat = await fs.lstat(fullPath);
+      const { size, birthtime } = await fs.lstat(fullPath);
       let type = 'file';
       if (dirent.isDirectory()) {
         type = 'directory';
       } else if (dirent.isSymbolicLink()) {
         type = 'symlink';
       }
-      const birthtime = stat.birthtime;
-      const month = monthFormatter.format(birthtime);
-      const day = dayFormatter.format(birthtime);
-      const time = timeFormatter.format(birthtime);
-      const info = {
+
+      return {
         type,
-        size: stat.size,
-        date: `${month} ${day} ${time}`,
+        size,
+        date: `${dateFormatter.format(birthtime)}`,
         name: dirent.name,
       };
-      return info;
     })
   );
   console.table(results);
