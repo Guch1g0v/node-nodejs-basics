caf3863c3514b28b1cad7300cdd40a3c62d109a6
guch1g0v <lanov64@gmail.com>
2025-12-18 20:46:18 +0500

    feat: add env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-  // Write your code here
+  const prefix = 'RSS_';
+  const envVarsWithPrefix = Object.entries(process.env)
+    .filter(([key]) => key.startsWith(prefix));
+  const formattedEnvPairsStr = envVarsWithPrefix
+    .map(([key, value]) => `${key}=${value}`).join(';');
+    
+  console.log(formattedEnvPairsStr);
 };
 
 parseEnv();
